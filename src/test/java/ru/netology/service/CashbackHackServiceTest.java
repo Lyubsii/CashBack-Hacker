a4e3d687d7c4594084eac52589b946916da39208
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-03-09 20:10:03 +0300

    add tests in brunch testing commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,42 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
 
+    @Test
+    public void missingOneTest(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void spent1000Test(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountMoreBoundary(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+
+
 }
+
